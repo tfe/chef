897918a9854a7281bdf9b6de7c6df19ebc46d0a7
Todd Eichel <todd@toddeichel.com>
2008-07-17 06:56:11 +0000

    Added date column to Product, installed autotest.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   
   active_scaffold :product do |config|
-    config.columns = [:name, :price, :short_description, :description, :image_url_small, :image_url_large]
+    config.columns = [:name, :price, :date, :short_description, :description, :image_url_small, :image_url_large]
     config.subform.columns = [:name, :price]
     list.columns.exclude [:image_url_small, :image_url_large]
-    list.sorting = {:created_at => :desc}
+    list.sorting = {:date => :desc}
   end
   
 end
