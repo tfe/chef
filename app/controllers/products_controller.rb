4e07c157c8a5dea82f189303afdfe0e978f7c35c
Todd Eichel <todd@toddeichel.com>
2008-07-27 05:28:53 +0000

    Showing more columns in the active scaffolded views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   
   active_scaffold :product do |config|
-    config.columns = [:name, :price, :date, :short_description, :description, :image_url_small, :image_url_large]
+    config.columns = [:name, :sku, :price, :date, :short_description, :description, :image_url_small, :image_url_large]
     config.subform.columns = [:name, :price]
     list.columns.exclude [:image_url_small, :image_url_large]
     list.sorting = {:date => :desc}
